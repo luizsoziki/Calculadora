94e256c9c8a1c1c5f6cbd33284bc4be9ccb78298
luizsoziki <luiz9920@hotmail.com>
2023-06-03 22:17:37 -0300

    Meu Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("O Resultado Da Soma De 5+2 é:");
+            var resultadoDaSoma = Calcular(2, 5);
+            Console.WriteLine(resultadoDaSoma);
+        }
+        public static int Calcular(int numero1, int numero2) 
+        { 
+            return numero1+numero2; 
+        }
+    }
+}
